50e2dcca860b38b563d6f13c2fee477bddb7aca5
fluteds <fluteds@users.noreply.github.com>
2021-03-16 14:17:57 +0000

    Update plugmention.js

--- plugmention.js
+++ plugmention.js
@@ -10,8 +10,8 @@
 // ==/UserScript==
 
 // UI Sounds
-UI_SOUND_URL = 'https://s3.amazonaws.com/assets.turntable.fm/web/sounds/';
-UI_SOUND_CHAT = UI_SOUND_URL + 'chat.mp3';
-UI_SOUND_MENTION = 'https://www.dropbox.com/s/i9inn3j2p9hm8ns/badoop.mp3?dl=1';
-UI_SOUND_PM = UI_SOUND_URL + 'pm.mp3';
-UI_SOUND_ENDSONG = UI_SOUND_URL + 'endsong.mp3';
+UI_SOUND_URL = 'https://s3.amazonaws.com/assets.turntable.fm/web/sounds/'; // Original sound assets for tt.fm
+UI_SOUND_CHAT = UI_SOUND_URL + 'chat.mp3'; // New chat message 
+UI_SOUND_MENTION = 'https://www.dropbox.com/s/i9inn3j2p9hm8ns/badoop.mp3?dl=1'; // Pulls mp3 from Dropbox. TODO: host the mp3 on a faster server.
+UI_SOUND_PM = UI_SOUND_URL + 'pm.mp3'; // New PM 
+UI_SOUND_ENDSONG = UI_SOUND_URL + 'endsong.mp3'; // Shuffling DJ
