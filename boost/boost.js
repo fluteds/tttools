8a580120eb5fa515c511cc8c928b7d9e32181460
fluteds <fluteds@users.noreply.github.com>
2021-04-10 13:13:19 +0100

    Boost updates

--- boost/boost.js
+++ boost/boost.js
@@ -1,5 +1,4 @@
 // TODO Enable awesome/meh buttons
-// Add background?
 
 function addStyleString(str) {
     var node = document.createElement('style');
@@ -7,8 +6,10 @@ function addStyleString(str) {
     document.body.appendChild(node);
 }
 
-addStyleString('#room-view { display: none !important }');
+// addStyleString('#room-view { display: none !important }');
 addStyleString('.screen-content { display: none !important }');
+addStyleString('.speech-bubble { display: none !important }');
+addStyleString('#room-view>canvas:nth-child(4) { display: none !important }');
 
 setTimeout(function () {
     $('#video-only-dj').click(function () {
